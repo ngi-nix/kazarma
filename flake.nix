4b4682eb95cdcdf617e70f0f1c59db92e49ed468
julienurraca@gmail.com <julienurraca@gmail.com>
2021-10-03 12:02:37 -0500

    start adding services config

--- flake.nix
+++ flake.nix
@@ -51,15 +51,76 @@
       hydraJobs = { inherit (legacyPackages) kazarma; };
       checks = { inherit (legacyPackages) kazarma; };
       nixosModules.kazarma =
-        { lib, config, ... }:
+        { nixpkgs, lib, config, ... }:
             with lib;
             let
               cfg = config.services.kazarma;
             in {
+              options = {
+                services.kazarma = {
+                  enable = mkEnableOption "Enable Kazarma, a Matrix bridge to ActivityPub";
+                  host = mkOption {
+                    type = types.str;
+                    default = "127.0.0.1";
+                    description = "Host for the Kazarma application, used to generate URLs.";
+                  };
+                  databaseUrl = mkOption {
+                    type = types.str;
+                    default = "ecto://kazarma:postgres@postgres_kazarma/kazarma";
+                    description = "URL with ecto:// scheme (ecto://user:password@host:database),";
+                  };
+                  secretKeyBase = mkOption {
+                    type = types.str;
+                    default = null;
+                    description = "Phoenix's secret key base, used to sign session cookies. With Mix and Phoenix, it can be easily generated with mix phx.gen.secret.";
+                  };
+                  homeserverToken = mkOption {
+                    type = types.str;
+                    default = null;
+                    description = "Token defined in the application service configuration file, will be used to authenticate the Matrix server against Kazarma.";
+                  };
+                  accessToken = mkOption {
+                    type = types.str;
+                    default = null;
+                    description = "Token defined in the application service configuration file, will be used to authenticate Kazarma against the Matrix server.";
+                  };
+                  matrixUrl = mkOption {
+                    type = types.str;
+                    default = "https://127.0.0.1:8008/matrix/";
+                    description = "URL to the Matrix server.";
+                  };
+                  activityPubDomain = mkOption {
+                    type = types.str;
+                    default = null;
+                    description = "";
+                  };
+                  puppetPrefix = mkOption {
+                    type = types.str;
+                    default = "ap_";
+                    description = "Username prefix for Matrix puppet users that correspond to ActivityPub real actors.";
+                  };
+                  bridgeRemote = mkOption {
+                    type = types.bool;
+                    default = false;
+                    description = "Whether Kazarma should bridge Matrix users from different homeservers (than the one beside Kazarma), to the ActivityPub network.";
+                  };
+                  htmlSearch = mkOption {
+                    type = types.bool;
+                    default = true;
+                    description = "Whether to show the search field on Kazarma HTML pages.";
+                  };
+                  htmlAP = mkOption {
+                    type = types.bool;
+                    default = true;
+                    description = "Whether to display profiles for ActivityPub actors. It can help Matrix users to get the (puppet) Matrix ID to reach an ActivityPub actor.";
+                  };
+                };
+              };
+
               config = mkIf cfg.enable {
                     nixpkgs.overlays = [ self.overlay ];
 
-                    systemd.packages = [ defaultPackage ];
+                    systemd.packages = [ defaultPackage matrix-synapse element-desktop pleroma-otp ];
 
                     systemd.services.kazarma = {
                       path = [ defaultPackage ];
@@ -71,7 +132,43 @@
                         wantedBy = [ "default.target" ];
                       };
                     };
+
+                    services.matrix-synapse = {
+                      enable = true;
+                      package = nixpkgs.matrix-synapse;
+                    };
+
+                    services.element-desktop = {
+                      enable = true;
+                      package = nixpkgs.element-desktop;
+                    };
+
+                    services.pleroma-otp = {
+                      enable = true;
+                      package = nixpkgs.pleroma-otp;
+                    };
               };
+              #   # A VM test of the NixOS module.
+              vmTest =
+                with import (nixpkgs + "/nixos/lib/testing-python.nix")
+                  {
+                    inherit system;
+                  };
+
+                makeTest {
+                  nodes = {
+                    client = { ... }: {
+                      imports = [ self.nixosModules.kazarma ];
+                    };
+                  };
+
+                  testScript =
+                    ''
+                      start_all()
+                      client.wait_for_unit("default.target")
+                      client.succeed("hello")
+                    '';
+                 };
             };
     }) // { overlay = overlay ;};
 }
